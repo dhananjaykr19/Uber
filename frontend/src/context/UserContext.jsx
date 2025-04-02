e6e7278bd90256efa202ab011b4d97852c8486cd
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-04-02 23:43:10 +0530

    initialize frontend with React, Vite, and Tailwind CSS setup

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,25 @@
+import React, { createContext} from 'react'
+import { useState } from 'react';
+export const UserDataContext = createContext();
+
+const UserContext = ({children}) => {
+
+    const [user, setUser] = useState(
+        {
+            email : '',
+            fullname : {
+                firstname : '',
+                lastname : ''
+            }
+        }
+    )
+    return (
+        <div>
+            <UserDataContext.Provider value={user}>
+                {children}
+            </UserDataContext.Provider>
+        </div>
+    )
+}
+
+export default UserContext
